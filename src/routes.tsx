28ef8ca0f8e6486af9c08210009d794dc815fbf0
Miguel Hernández <miguelhdzch@gmail.com>
2024-04-18 13:19:34 -0600

    Add padding to page wrapper

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from "pages/home-page/HomePage";
-import ProductList from "pages/products/product-list/ProductList";
-import ProductDetail from "pages/products/product-detail/ProductDetail";
+import { Route, Routes } from 'react-router-dom'
+import HomePage from 'pages/home-page/HomePage'
+import ProductList from 'pages/products/product-list/ProductList'
+import ProductDetail from 'pages/products/product-detail/ProductDetail'
 
 const AppRoutes = () => {
-    return (
-
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/productos" element={<ProductList />} />
-                <Route path="/productos/:productId" element={<ProductDetail />} />
-            </Routes>
-
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/productos" element={<ProductList />} />
+      <Route path="/productos/:productId" element={<ProductDetail />} />
+    </Routes>
+  )
 }
 
 export default AppRoutes
