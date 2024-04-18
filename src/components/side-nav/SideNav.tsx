3b2001a9bd780a72b4aedd1a43acbf6d1a849390
Miguel Hernández <miguelhdzch@gmail.com>
2024-04-18 00:06:24 -0600

    Add side nav menu with logo

--- src/components/side-nav/SideNav.tsx
+++ src/components/side-nav/SideNav.tsx
@@ -1,26 +1,69 @@
-import {useState} from "react";
+import Logo from 'assets/img/logo.png'
+import {Menu, MenuProps} from "antd";
+import {CodeSandboxOutlined, DashboardOutlined, EyeOutlined, MailOutlined, ShoppingCartOutlined} from "@ant-design/icons";
+import React from "react";
 
+type MenuItem = Required<MenuProps>['items'][number];
+
+const getItem = (
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+    type?: 'group',
+): MenuItem => {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    } as MenuItem;
+}
 const SideNav = () => {
-    const [open, setOpen] = useState(true)
 
-    const toggleNav = () => {
-        setOpen(!open)
-    }
+
+    const items: MenuProps['items'] = [
+        getItem('Inicio', 'home', <DashboardOutlined/>),
+
+        getItem('Inventario', 'stock', <MailOutlined/>, [
+            getItem('Lista de productos', 'stock-1'),
+            getItem('Añadir productos', 'stock-2'),
+            getItem('Lista de categorías', 'stock-3'),
+            getItem('Añadir categorías', 'stock-4'),
+        ]),
+
+        getItem('Finanzas', 'finance', <CodeSandboxOutlined/>, [
+            getItem('Lista de cotizaciones', 'finance-1'),
+            getItem('Añadir cotizacion', 'finance-2'),
+            getItem('Lista de ventas', 'finance-3'),
+            getItem('Lista de gastos', 'finance-4'),
+            getItem('Añadir gasto', 'finance-5'),
+        ]),
+
+        getItem('Personal', 'employees', <ShoppingCartOutlined/>, [
+            getItem('Lista de empleados', 'employees-1'),
+            getItem('Añadir empleado', 'employees-2'),
+            getItem('Lista de clientes', 'employees-3'),
+            getItem('Añadir cliente', 'employees-4'),
+        ]),
+
+        getItem('Producción', 'prod', <EyeOutlined/>, [
+            getItem('Estado de producción', 'prod-1'),
+        ]),
+    ];
+
 
     return (
         <div className='w-[16rem]'>
-            <button onClick={toggleNav}>hello</button>
+            <div className='h-16 px-4 py-2'>
+                <img src={Logo} alt="Ink Sports" className='h-10'/>
+            </div>
             <div>
-                <div>ñklsbksbk</div>
-                <div>ñklsbksbk</div>
-                <div>ñklsbksbk</div>
-                <div>ñklsbksbk</div>
-                <div>ñklsbksbk</div>
-                <div>ñklsbksbk</div>
-                <div>ñklsbksbk</div>
-                <div>ñklsbksbk</div>
-                <div>ñklsbksbk</div>
-                <div>ñklsbksbk</div>
+                <Menu
+                    mode="inline"
+                    items={items}
+                />
             </div>
         </div>
     )
--- src/components/top-nav/TopNav.tsx
+++ src/components/top-nav/TopNav.tsx
@@ -1,7 +1,11 @@
 const TopNav = () => {
     return (
-        <div className='h-14'>
-            hello
+        <div className='h-16 px-5 flex flex-row items-center justify-end'>
+            <div className='flex items-center gap-5'>
+                <div>search</div>
+                <div>notifications</div>
+                <div>profile</div>
+            </div>
         </div>
     )
 }
