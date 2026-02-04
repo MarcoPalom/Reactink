9ed562991a3b38093f4394370501380900df5a67
Eduardo Gallegos <galledmz@gmail.com>
2026-02-04 13:56:26 -0600

    marco picate el culo we jajjaja

--- src/routes.tsx
+++ src/routes.tsx
@@ -11,7 +11,7 @@ import Login from 'pages/Login';
 import Production from 'pages/Production/Production';
 import DebtList from 'pages/finance/debt-list/DebtList';
 import CuttingOrderList from 'pages/finance/cuttingorder-list/CuttingOrderList';
-import DesignArea from 'pages/home-page/DesingArea';
+import DesingList from 'pages/finance/desing-list/DesingList';
 
 const AppRoutes = () => {
     return (
@@ -25,7 +25,7 @@ const AppRoutes = () => {
                 <Route path='/finanzas/gastos' element={<ExpenseList/>}/>
                 <Route path='/finanzas/deudas' element={<DebtList/>}/>
                 <Route path='/finanzas/cuttingorders' element={<CuttingOrderList/>}/>
-                <Route path='/finanzas/desinglist' element={<DesignArea/>}/>
+                <Route path='/finanzas/desinglist' element={<DesingList/>}/>
                 <Route path='/personal/empleados' element={<EmployeList/>}/>
                 <Route path='/personal/clientes' element={<ClientList/>}/>
                 <Route path='/produccion' element={<Production/>}/>
